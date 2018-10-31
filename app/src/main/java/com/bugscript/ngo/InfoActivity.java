2717b9f453a68bebd157d66d2caf15ae331d3bc3
Syam Sundar K <k.syamsundar21@gmail.com>
2018-10-31 21:05:24 +0530

    Initial Intro screen added

---
+++ app/src/main/java/com/bugscript/ngo/InfoActivity.java
@@ -0,0 +1,13 @@
+package com.bugscript.ngo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class InfoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_info);
+    }
+}
